de029cc5da78906ecd38362e944fd9d54e70dd5c
donkats-dbt <78106637+donkats-dbt@users.noreply.github.com>
2025-06-30 11:32:05 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,27 +5,34 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/form", methods=["GET", "POST"])
+def form():
     if request.method == "POST":
-        
         input_type = request.form.get("input_type")
         user_input = request.form.get("user_input")
 
         results = analyze_url_or_text(user_input, input_type)
-        
+
         pdf_path = create_pdf(results)
-        
+
         return render_template("results.html", results=results, pdf_path=pdf_path)
- 
+
     return render_template("form.html")
- 
+    
+@app.route("/")
+def landing():
+    return render_template("landing.html")    
+    
+@app.route("/pay")
+def pay():
+    return render_template("pay.html")
+    
 # added start
 @app.route("/session_completed")
 def session_completed():
     return render_template("session_completed.html")
-# added end 
- 
+# added end     
+
 @app.route("/download")
 def download():
     return send_file("output/donkats_seo_report.pdf", as_attachment=True)
